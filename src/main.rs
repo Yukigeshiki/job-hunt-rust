25800671c5a399b8743b03f95053626f76ff19e3
Laurence <laurence@robothouse.io>
2023-01-26 13:04:34 +0200

    update repository index types

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use jobhunt::repository::Jobs;
+use jobhunt::repository::Repository;
 use crate::scraper::Scraper;
 use jobhunt::scraper;
 use jobhunt::site::{Site, Web3Jobs};
 
 fn main() {
-    let mut jobs = Jobs::default();
+    let mut repo = Repository::default();
     {
         let mut web3_jobs = Web3Jobs::new();
-        jobs
-            .aggregate_to_all(
+        repo
+            .import_into_repository(
                 vec![
                     &mut web3_jobs.scrape().unwrap().jobs
                 ]
@@ -17,8 +17,9 @@ fn main() {
             .index();
     }
 
-    println!("{:?}", jobs.all);
-    println!("{:?}", jobs.date);
-    println!("{:?}", jobs.company);
-    println!("{:?}", jobs.job_type);
+    println!("{:?}", repo.all);
+    println!("{:?}", repo.date);
+    println!("{:?}", repo.company);
+    println!("{:?}", repo.skill);
+    println!("{:?}", repo.level);
 }
